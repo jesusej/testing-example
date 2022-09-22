08ce7a23c4624095b4a60dd653a3d6a2ab3a3022
Jesus Enríquez Jaime <jesus-e98@hotmail.com>
2022-09-22 12:08:27 -0700

    Setting functions

---
+++ src/Counter.js
@@ -0,0 +1,36 @@
+import { useState } from 'react';
+
+function Counter({ initialCount }) {
+  const [count, setCount] = useState(initialCount)
+
+  const increment = () => {
+    setCount((prev) = prev + 1);
+  }
+
+  const decrement = () => {
+    setCount((prev) => prev - 1);
+  }
+
+  const restart = () => {
+    setCount(initialCount);
+  }
+
+  const switchSigns = () => {
+    setCount((prev) => prev *-1);
+  }
+  
+  return (
+    <div>
+      <h1>
+        Count: <h3>{count}</h3>
+      </h1>
+      <div>
+        <button onClick={increment}>Increment</button>
+        <button onClick={decrement}>Decrement</button>
+        <button onClick={restart}>Restart</button>
+        <button onClick={switchSigns}>Switch Signs</button>
+      </div>
+    </div>
+  )
+}
+export default Counter
